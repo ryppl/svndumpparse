6594759905cd2a6f6ed71c49a70a70f0d61513ae
Dave Abrahams <dave@boostpro.com>
2013-01-09 10:31:47 -0500

    Record a compile command in .dir-locals.el

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables
+;;; See Info node `(emacs) Directory Variables' for more information.
+
+(("."
+  (compile-command . "mkdir -p /tmp/bld/modhist && cd /tmp/bld/modhist && cmake -f  -G Ninja ~/src/ryppl/modhist && ninja")))
